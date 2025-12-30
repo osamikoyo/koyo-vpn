39cf20fa00b7fa2356ecf5de28e6db70a3379fad
osamikoyo <v.pylayev@mail.ru>
2025-12-30 06:05:24 +0500

    add server config

---
+++ internal/config/server.go
@@ -0,0 +1,12 @@
+package config
+
+type Keys struct{
+	Self string `yaml:"self"`
+	Client string `yaml:"client"`
+}
+
+type ServerConfig struct{
+	UDPAddr string `yaml:"udp_addr"`
+	DeviceName string `yaml:"device_nane"`
+	Keys Keys `yaml:"keys"`
+}
