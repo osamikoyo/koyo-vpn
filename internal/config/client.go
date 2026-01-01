d79d69c8ad421998123d23e4678741231d235df0
osamikoyo <v.pylayev@mail.ru>
2026-01-01 10:01:51 +0500

    update client config

--- internal/config/client.go
+++ internal/config/client.go
@@ -1,7 +1,7 @@
 package config
 
-type ClientConfig struct{
-	SelfKey string `yaml:"self_key"`
-	DeviceName string `yaml:"device_name"`
+type ClientConfig struct {
+	SelfKey       string `yaml:"self_key"`
+	DeviceName    string `yaml:"device_name"`
 	ServerUDPAddr string `yaml:"server_udp_addr"`
 }
