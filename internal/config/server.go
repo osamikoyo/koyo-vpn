de4c55aaede2b2068b7381a2298919c5e52a68e8
osamikoyo <v.pylayev@mail.ru>
2026-01-01 10:02:10 +0500

    update server config

--- internal/config/server.go
+++ internal/config/server.go
@@ -1,12 +1,12 @@
 package config
 
-type Keys struct{
-	Self string `yaml:"self"`
+type Keys struct {
+	Self   string `yaml:"self"`
 	Client string `yaml:"client"`
 }
 
-type ServerConfig struct{
-	UDPAddr string `yaml:"udp_addr"`
+type ServerConfig struct {
+	UDPAddr    string `yaml:"udp_addr"`
 	DeviceName string `yaml:"device_nane"`
-	Keys Keys `yaml:"keys"`
+	Keys       Keys   `yaml:"keys"`
 }
