4bf853b9f607ab19a4a27752efb00d87ffb2ab14
osamikoyo <v.pylayev@mail.ru>
2026-01-01 10:02:00 +0500

    add generic config

--- internal/config/generic.go
+++ internal/config/generic.go
@@ -7,41 +7,41 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type GenericConfig interface{
+type GenericConfig interface {
 	*ClientConfig | *ServerConfig
 }
 
 func NewConfig[T GenericConfig](cType, path string) (T, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed open config file: %w", err)
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed open config file: %w", err)
+	}
+	defer file.Close()
 
-    var zero T
+	var zero T
 
-    switch any(zero).(type) {
-    case *ServerConfig:
-        if cType != "server" {
-            return zero, fmt.Errorf("expected server config")
-        }
-        cfg := new(ServerConfig)
-        if err := yaml.NewDecoder(file).Decode(cfg); err != nil {
-            return zero, fmt.Errorf("failed decode: %w", err)
-        }
-        return any(cfg).(T), nil
+	switch any(zero).(type) {
+	case *ServerConfig:
+		if cType != "server" {
+			return zero, fmt.Errorf("expected server config")
+		}
+		cfg := new(ServerConfig)
+		if err := yaml.NewDecoder(file).Decode(cfg); err != nil {
+			return zero, fmt.Errorf("failed decode: %w", err)
+		}
+		return any(cfg).(T), nil
 
-    case *ClientConfig:
-        if cType != "client" {
-            return zero, fmt.Errorf("expected client config")
-        }
-        cfg := new(ClientConfig)
-        if err := yaml.NewDecoder(file).Decode(cfg); err != nil {
-            return zero, fmt.Errorf("failed decode: %w", err)
-        }
-        return any(cfg).(T), nil
+	case *ClientConfig:
+		if cType != "client" {
+			return zero, fmt.Errorf("expected client config")
+		}
+		cfg := new(ClientConfig)
+		if err := yaml.NewDecoder(file).Decode(cfg); err != nil {
+			return zero, fmt.Errorf("failed decode: %w", err)
+		}
+		return any(cfg).(T), nil
 
-    default:
-        return zero, fmt.Errorf("unsupported config type")
-    }
+	default:
+		return zero, fmt.Errorf("unsupported config type")
+	}
 }
