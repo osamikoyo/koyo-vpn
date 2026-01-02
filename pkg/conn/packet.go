f506950ee37dc2ae882dfce4f77d0cb65d154fca
osamikoyo <v.pylayev@mail.ru>
2026-01-02 07:57:08 +0500

    update packet

--- pkg/conn/packet.go
+++ pkg/conn/packet.go
@@ -3,15 +3,19 @@ package conn
 import "net"
 
 type Packet struct {
-    Buf      []byte         
-    Addr     net.UDPAddr    
-    Type     uint8         
+	Buf  []byte
+	Addr net.UDPAddr
+	Type uint8
+	// 1 is hs to server
+	// 2 is hs from server
+	// 3 is trafic to server
+	// 4 is trafic from server
 }
 
 func ParsePacket(buf []byte, addr net.UDPAddr) *Packet {
-    return &Packet{
-        Buf: buf[0:],
-        Addr: addr,
-        Type: buf[0],
-    }
+	return &Packet{
+		Buf:  buf[0:],
+		Addr: addr,
+		Type: buf[0],
+	}
 }
