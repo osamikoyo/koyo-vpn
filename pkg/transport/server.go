d1ca0d6dd52a8bdd3137a872a792f697c81469ed
osamikoyo <v.pylayev@mail.ru>
2026-01-09 08:08:16 +0500

    update server transport

--- pkg/transport/server.go
+++ pkg/transport/server.go
@@ -6,8 +6,12 @@ import (
 	"koyo-vpn/pkg/chifer"
 	"koyo-vpn/pkg/conn"
 	"koyo-vpn/pkg/device"
+	"koyo-vpn/pkg/errors"
 	"koyo-vpn/pkg/handshake"
+	"koyo-vpn/pkg/logger"
 	"net"
+
+	"go.uber.org/zap"
 )
 
 type ServerSideTransport struct {
@@ -21,9 +25,11 @@ type ServerSideTransport struct {
 	conn      *conn.Conn
 	device    *device.Device
 	selfAddr  *net.UDPAddr
+
+	logger *logger.Logger
 }
 
-func newServerSideTransport(deviceName, selfAddr, remoteAddr, remoteKey, selfKey string, nonce []byte) (*ServerSideTransport, error) {
+func newServerSideTransport(logger *logger.Logger, deviceName, selfAddr, remoteAddr, remoteKey, selfKey string, nonce []byte) (*ServerSideTransport, error) {
 	var (
 		fromTUN chan []byte
 		toTUN   chan []byte
@@ -34,20 +40,39 @@ func newServerSideTransport(deviceName, selfAddr, remoteAddr, remoteKey, selfKey
 
 	selfUdpAddr, err := net.ResolveUDPAddr("udp", selfAddr)
 	if err != nil {
+		logger.Error("failed resolve udp addr",
+			zap.String("addr", selfAddr),
+			zap.Error(err))
+
 		return nil, fmt.Errorf("failed resolve udp addr %s: %w", selfAddr, err)
 	}
 
 	device, err := device.NewDevice(fromTUN, toTUN, deviceName)
 	if err != nil {
+		logger.Error("failed create device",
+			zap.String("name", deviceName),
+			zap.Error(err))
+
 		return nil, fmt.Errorf("failed setup device: %w", err)
 	}
 
 	conn, err := conn.NewConn(selfAddr, remoteAddr, toConn, fromConn)
 	if err != nil {
+		logger.Error("failed setup conn",
+			zap.String("self_addr", selfAddr),
+			zap.String("remote_addr", remoteAddr),
+			zap.Error(err))
+
 		return nil, fmt.Errorf("failed setup conn: %w", err)
 	}
 
 	handshake := handshake.NewHandShakeRouter(remoteKey, selfKey)
+
+	logger.Info("transport setupped successfully",
+		zap.String("device_name", deviceName),
+		zap.String("self_addr", selfAddr),
+		zap.String("remote_addr", remoteAddr),
+		zap.String("selfKey", selfKey))
 
 	return &ServerSideTransport{
 		device:         device,
@@ -61,36 +86,41 @@ func newServerSideTransport(deviceName, selfAddr, remoteAddr, remoteKey, selfKey
 	}, nil
 }
 
-func (t *ServerSideTransport) StartAsync(ctx context.Context) chan error {
-	errors := make(chan error)
+func (t *ServerSideTransport) StartAsync(ctx context.Context) chan errors.Error {
+	errors := make(chan errors.Error)
+
+	go t.device.StartAsync(ctx, errors)
+	go t.conn.StartAsync(ctx, errors)
 
 	go t.startReadingFromConn(ctx, errors)
 	go t.startReadingFromDevice(ctx, errors)
 
+	t.logger.Info("transport started successfully")
+
 	return errors
 }
 
-func (t *ServerSideTransport) startReadingFromDevice(ctx context.Context, errors chan error) {
+func (t *ServerSideTransport) startReadingFromDevice(ctx context.Context, errs chan errors.Error) {
 	for {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			return
-		case buf := <- t.deviceOutbound:
-			if err := t.routeFromDevice(ctx, buf);err != nil{
-				errors <- err
+		case buf := <-t.deviceOutbound:
+			if err := t.routeFromDevice(ctx, buf); err != nil {
+				errs <- errors.NewError("transport", err.Error(), false)
 			}
 		}
 	}
 }
 
-func (t *ServerSideTransport) startReadingFromConn(ctx context.Context, errors chan error) {
+func (t *ServerSideTransport) startReadingFromConn(ctx context.Context, errs chan errors.Error) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case packet := <-t.connOutbound:
 			if err := t.routeFromConn(ctx, &packet); err != nil {
-				errors <- err
+				errs <- errors.NewError("transport", err.Error(), false)
 			}
 		}
 	}
@@ -105,6 +135,9 @@ func (t *ServerSideTransport) routeFromDevice(_ context.Context, buf []byte) err
 
 	encryptedbuf, err := chifer.Encrypt(buf, key, t.nonce)
 	if err != nil {
+		t.logger.Error("failed encrypt buffer",
+			zap.Error(err))
+
 		return fmt.Errorf("failed ecrypt buffer: %w", err)
 	}
 
