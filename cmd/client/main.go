bda8ea24a9571462aaaff062825c551c428878bb
osamikoyo <v.pylayev@mail.ru>
2026-01-10 08:52:42 +0500

    update client main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"koyo-vpn/internal/config"
+	"koyo-vpn/internal/core"
+	"koyo-vpn/pkg/logger"
+	"os"
+)
+
+func main() {
+	logger.Init(logger.Config{
+		AppName:   "vpn-client",
+		AddCaller: false,
+		LogFile:   "vpn-client.log",
+		LogLevel:  "debug",
+	})
+
+	logger := logger.Get()
+
+	logger.Info("setup client...")
+
+	cfg, err := getCfg(logger)
+	if err != nil{
+		return
+	}
+
+	
+
+}
+
+func getCfg(logger *logger.Logger) (*config.ClientConfig, error) {
+	path := "client.yaml"
+
+	for i, arg := range os.Args {
+		if arg == "--config" {
+			path = os.Args[i+1]
+		}
+	}
+
+	cfg, err := config.NewConfig[*config.ClientConfig]("client", path)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
