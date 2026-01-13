f365c9d06ca64cc9458c6cb95c6b58def99f8329
osamikoyo <v.pylayev@mail.ru>
2026-01-13 06:50:56 +0500

    update server main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"context"
+	"koyo-vpn/internal/config"
+	"koyo-vpn/internal/core"
+	"koyo-vpn/pkg/logger"
+	"os"
+	"os/signal"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger.Init(logger.Config{
+		AppName:   "vpn-server",
+		AddCaller: false,
+		LogFile:   "vpn-server.log",
+		LogLevel:  "debug",
+	})
+
+	logger := logger.Get()
+
+	logger.Info("setup server...")
+
+	cfg, err := getCfg(logger)
+	if err != nil {
+		return
+	}
+
+	core, err := core.SetupClientCore(cfg, logger)
+	if err != nil {
+		logger.Error("failed setup client core",
+			zap.Error(err))
+	}
+
+	logger.Info("starting client")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	core.Start(ctx)
+}
+
+func getCfg(logger *logger.Logger) (*config.ClientConfig, error) {
+	path := "client.yaml"
+
+	for i, arg := range os.Args {
+		if arg == "--config" {
+			path = os.Args[i+1]
+		}
+	}
+
+	cfg, err := config.NewConfig[*config.ClientConfig]("client", path)
+	if err != nil {
+		logger.Error("failed get config",
+			zap.String("path", path),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	return cfg, nil
+}
