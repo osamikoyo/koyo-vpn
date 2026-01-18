25bf9947ca0939ff4618c7cc827236e6922d206b
osamikoyo <v.pylayev@mail.ru>
2026-01-18 08:23:13 +0500

    update server config

--- internal/config/server.go
+++ internal/config/server.go
@@ -1,14 +1,46 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"koyo-vpn/pkg/logger"
+
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+)
+
 type Keys struct {
 	Self   string `yaml:"self"`
 	Remote string `yaml:"remote"`
 }
 
 type ServerConfig struct {
-	Nonce string `yaml:"nonce"`
-	DeviceName string `yaml:"device_nane"`
-	
-	Addrs Addrs `yaml:"addr"`
-	Keys       Keys   `yaml:"keys"`
+	DeviceName string `yaml:"device_name"`
+
+	Addrs Addrs `yaml:"addrs"`
+	Keys  Keys  `yaml:"keys"`
+}
+
+func LoadServerConfig(logger *logger.Logger, path string) (*ServerConfig, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		logger.Error("failed open config path",
+			zap.String("path", path),
+			zap.Error(err))
+
+		return nil, fmt.Errorf("failed open config file: %s: %w", path, err)
+	}
+
+	var cfg ServerConfig
+
+	if err = yaml.NewDecoder(file).Decode(&cfg); err != nil {
+		logger.Error("failed load server config",
+			zap.String("path", path),
+			zap.Error(err))
+
+		return nil, fmt.Errorf("failed load server config: %w", err)
+	}
+
+	return nil, nil
 }
